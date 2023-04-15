0fd5de1704bb6adabefc114bc787c3961a6a3958
lucioalmeidastockio <lucio.almeida@stockio.com.br>
2023-04-15 05:18:54 -0300

    fix: mapped exception supper construct details message fix

--- src/main/java/br/com/stockio/use_cases/io/exceptions/NotBlankAnnotationOnWrongTypeException.java
+++ src/main/java/br/com/stockio/use_cases/io/exceptions/NotBlankAnnotationOnWrongTypeException.java
@@ -4,6 +4,6 @@ import br.com.stockio.mapped_exceptions.specifics.InternalMappedException;
 
 public class NotBlankAnnotationOnWrongTypeException extends InternalMappedException {
     public NotBlankAnnotationOnWrongTypeException(String name) {
-        super("The not-blank annotation should only be applied upon String fields. There is an issue about it at field '" + name + "'");
+        super("The not-blank annotation should only be applied upon String fields.", "There is an issue about it at field '" + name + "'");
     }
 }
--- src/main/java/br/com/stockio/use_cases/io/exceptions/ValidInnerPropertiesAnnotationOnWrongTypeException.java
+++ src/main/java/br/com/stockio/use_cases/io/exceptions/ValidInnerPropertiesAnnotationOnWrongTypeException.java
@@ -5,7 +5,7 @@ import br.com.stockio.mapped_exceptions.specifics.InternalMappedException;
 
 public class ValidInnerPropertiesAnnotationOnWrongTypeException extends InternalMappedException {
     public ValidInnerPropertiesAnnotationOnWrongTypeException(String name) {
-        super("The valid-inner-properties annotation should only be applied upon T (T -> T extends UseCaseInput) fields. There is an issue about it" +
+        super("The valid-inner-properties annotation should only be applied upon T (T -> T extends UseCaseInput) fields.", "There is an issue about it" +
                 " at field '" + name + "'");
     }
 }
