49648210f067eb66ff7b6fab7964b317321a62fb
lucioalmeidastockio <lucio.almeida@stockio.com.br>
2023-04-16 02:58:07 -0300

    fix: caught bug in UseCaseException message setting

--- src/main/java/br/com/stockio/use_cases/exceptions/UseCaseExecutionException.java
+++ src/main/java/br/com/stockio/use_cases/exceptions/UseCaseExecutionException.java
@@ -6,8 +6,8 @@ import br.com.stockio.use_cases.UseCase;
 public class UseCaseExecutionException extends InternalMappedException {
     public UseCaseExecutionException(UseCase useCase, Exception unexpectedException) {
         super(
-                "Something went unexpectedly wrong while executing use case of '" + useCase.getUseCaseMetadata().getName() + "'. More details: " + unexpectedException.toString(),
-                "Something went unexpectedly wrong while executing use case of '" + useCase.getUseCaseMetadata().getName() + "'."
+                "Something went unexpectedly wrong while executing use case of '" + useCase.getUseCaseMetadata().getName() + "'",
+                "More details: " + unexpectedException
         );
     }
 }
