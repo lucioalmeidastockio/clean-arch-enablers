06db135faf65ea6758cb304532bcc46ba9bafe7c
lucioalmeidastockio <lucio.almeida@stockio.com.br>
2023-04-17 03:19:37 -0300

    use case specific types javadoc comments

--- src/main/java/br/com/stockio/use_cases/specifics/consumers/ConsumerUseCase.java
+++ src/main/java/br/com/stockio/use_cases/specifics/consumers/ConsumerUseCase.java
@@ -8,12 +8,31 @@ import br.com.stockio.use_cases.exceptions.UseCaseExecutionException;
 import br.com.stockio.use_cases.io.UseCaseInput;
 import br.com.stockio.use_cases.metadata.UseCaseMetadata;
 
+/**
+ * Specific type of UseCase which only has input but no output.
+ * @param <I> type of input
+ */
 public abstract class ConsumerUseCase <I extends UseCaseInput> extends UseCase {
 
     protected ConsumerUseCase(UseCaseMetadata useCaseMetadata, Logger logger) {
         super(useCaseMetadata, logger);
     }
 
+    /**
+     * Public method which triggers the execution of the ConsumerUseCase.
+     * It will internally call the method which keeps the core logic of the
+     * use case. If anything goes unexpectedly wrong during its execution,
+     * it will throw a UseCaseExecutionException instance with the description
+     * of what have gone wrong. If your use case implementation throws
+     * a MappedException instance, it will not intercede as it will consider
+     * the MappedException as part of the planned flow.
+     * Executing your use case with this method assures there will be
+     * automated log tracking control: the beginning and the ending of the
+     * use case execution will be logged, weather it ends successfully or not.
+     * However, you are still free to use your logger instance as you wish
+     * inside your use case implementations.
+     * @param input the input of the use case
+     */
     public void execute(I input){
         input.validateProperties();
         Trier.of(() ->  UseCaseProcessorFactory.of(this, input.getCorrelation(), this.logger).processUseCaseUsing(input))
@@ -21,6 +40,10 @@ public abstract class ConsumerUseCase <I extends UseCaseInput> extends UseCase {
                 .andExecuteTheAction();
     }
 
+    /**
+     * Internal method supposed to execute the core logic of the use case
+     * @param input input of the use case
+     */
     protected abstract void applyInternalLogic(I input);
 
 
--- src/main/java/br/com/stockio/use_cases/specifics/functions/FunctionUseCase.java
+++ src/main/java/br/com/stockio/use_cases/specifics/functions/FunctionUseCase.java
@@ -8,12 +8,33 @@ import br.com.stockio.use_cases.exceptions.UseCaseExecutionException;
 import br.com.stockio.use_cases.io.UseCaseInput;
 import br.com.stockio.use_cases.metadata.UseCaseMetadata;
 
+/**
+ * Specific type of UseCase which has both input and output
+ * @param <I> type of input
+ * @param <O> type of output
+ */
 public abstract class FunctionUseCase <I extends UseCaseInput, O> extends UseCase {
 
     protected FunctionUseCase(UseCaseMetadata useCaseMetadata, Logger logger) {
         super(useCaseMetadata, logger);
     }
 
+    /**
+     * Public method which triggers the execution of the FunctionUseCase.
+     * It will internally call the method which keeps the core logic of the
+     * use case. If anything goes unexpectedly wrong during its execution,
+     * it will throw a UseCaseExecutionException instance with the description
+     * of what have gone wrong. If your use case implementation throws
+     * a MappedException instance, it will not intercede as it will consider
+     * the MappedException as part of the planned flow.
+     * Executing your use case with this method assures there will be
+     * automated log tracking control: the beginning and the ending of the
+     * use case execution will be logged, weather it ends successfully or not.
+     * However, you are still free to use your logger instance as you wish
+     * inside your use case implementations.
+     * @param input the input of the use case
+     * @return the output of the use case
+     */
     public O execute(I input){
         input.validateProperties();
         return Trier.of(() ->  UseCaseProcessorFactory.of(this, input.getCorrelation(), this.logger).processUseCaseUsing(input))
@@ -21,6 +42,11 @@ public abstract class FunctionUseCase <I extends UseCaseInput, O> extends UseCas
                 .andExecuteTheAction();
     }
 
+    /**
+     * Internal method supposed to execute the core logic of the use case
+     * @param input input of the use case
+     * @return output of the use case
+     */
     protected abstract O applyInternalLogic(I input);
 
 }
