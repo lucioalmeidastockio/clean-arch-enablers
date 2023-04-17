35d2ed1fa4bec3ff87a0b0da6fa474d4a63ad353
lucioalmeidastockio <lucio.almeida@stockio.com.br>
2023-04-17 01:31:00 -0300

    feat: adding javadoc comments

--- src/main/java/br/com/stockio/entities/Entity.java
+++ src/main/java/br/com/stockio/entities/Entity.java
@@ -1,5 +1,10 @@
 package br.com.stockio.entities;
 
+/**
+ * This is a marker interface. It is for marking classes which will be
+ * used as entities of your application. Not persistence entities,
+ * business entities.
+ */
 public interface Entity {
 }
 
